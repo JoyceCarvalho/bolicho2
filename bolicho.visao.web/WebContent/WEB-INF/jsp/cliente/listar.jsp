7d2f81d195cebdfc955c03fc022663ffc7fdc6c5
Alunos <Alunos@PC-Lab3>
2017-05-04 22:18:12 -0300

    Atualização Banco de dados

--- bolicho.visao.web/WebContent/WEB-INF/jsp/cliente/listar.jsp
+++ bolicho.visao.web/WebContent/WEB-INF/jsp/cliente/listar.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Cliente</title>
 </head>
 <body>
 <h2>Listar Cliente</h2>
--- bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/ClienteController.java
+++ bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/ClienteController.java
@@ -1,11 +1,24 @@
 package br.edu.iffarroupilha.bolicho.visao.web;
 
 import br.com.caelum.vraptor.Resource;
+import br.edu.iffarroupilha.bolicho2.controle.ClienteControle;
+import br.edu.iffarroupilha.bolicho2.modelo.Cliente;
 
 @Resource
 public class ClienteController {
 	
 	public void listar(){
 		System.out.println("aqui vamos buscar os clientes!!");
+	}
+	
+	public void gravar(){
+		ClienteControle controle = new ClienteControle();
+		
+		//Objeto temporario
+		Cliente c = new Cliente();
+		c.setNome("João");
+		c.setCpf(1234);
+		
+		controle.gravar( c );
 	}
 }
